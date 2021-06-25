6f5e1edfe8f37c5f6ff69c3206d342a3832db9df
heswap <heswap@protonmail.com>
2021-06-25 15:58:38 +0800

    refactor: Update sdk

--- src/components/SearchModal/sorting.ts
+++ src/components/SearchModal/sorting.ts
@@ -1,4 +1,4 @@
-import { Token, TokenAmount } from '@pancakeswap-libs/sdk'
+import { Token, TokenAmount } from '@heswap/heswap-sdk'
 import { useMemo } from 'react'
 import { useAllTokenBalances } from '../../state/wallet/hooks'
 
--- src/state/application/reducer.test.ts
+++ src/state/application/reducer.test.ts
@@ -1,4 +1,4 @@
-import { ChainId } from '@pancakeswap-libs/sdk'
+import { ChainId } from '@heswap/heswap-sdk'
 import { createStore, Store } from 'redux'
 import { addPopup, removePopup, toggleSettingsMenu, toggleWalletModal, updateBlockNumber } from './actions'
 import reducer, { ApplicationState } from './reducer'
